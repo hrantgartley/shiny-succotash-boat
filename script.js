ff474577b92380cde1b4440c553fe69140833b08
hrantgartley <doyouevenjump@gmail.com>
2023-09-14 18:51:35 -0500

    good
    added fixer to empty input box if wrong input is given

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 // eslint-disable-next-line no-unused-vars
 function calculateSpeed() {
-    const weight = parseFloat(document.getElementById("weight").value);
-    const horsepower = parseFloat(document.getElementById("horsepower").value);
-    const constant = parseFloat(document.getElementById("boat-type").value);
-    const form = document.getElementById("calculator-form");
+  const weightInput = document.getElementById("weight");
+  const horsepowerInput = document.getElementById("horsepower");
+  const constant = parseFloat(document.getElementById("boat-type").value);
+  const form = document.getElementById("calculator-form");
 
-    const speed = (horsepower / weight) * constant;
-    if (weight > 45000) {
-        alert("Weight must be less than 45000 lbs");
-        return;
-    } else if (weight < 0) {
-        alert("Weight must be greater than 0 lbs");
-        return;
-    }
-    if (horsepower > 750) {
-        alert("Horsepower must be less than 750");
-        return;
-    } else if (horsepower < 0) {
-        alert("Horsepower must be greater than 0");
-        return;
-    }
+  const weight = parseFloat(weightInput.value);
+  const horsepower = parseFloat(horsepowerInput.value);
 
-    document.getElementById("calculated-speed").textContent =
-        speed.toFixed(2) + " knots";
-    form.reset();
+  const speed = (horsepower / weight) * constant;
+
+  if (weight > 45000) {
+    alert("Weight must be less than 45000 lbs");
+    weightInput.value = "";
+    return;
+  } else if (weight < 0) {
+    alert("Weight must be greater than 0 lbs");
+    weightInput.value = "";
+    return;
+  }
+
+  if (horsepower > 750) {
+    alert("Horsepower must be less than 750");
+    horsepowerInput.value = "";
+    return;
+  } else if (horsepower < 0) {
+    alert("Horsepower must be greater than 0");
+    horsepowerInput.value = "";
+    return;
+  }
+
+  document.getElementById("calculated-speed").textContent =
+    speed.toFixed(2) + " knots";
+  form.reset();
 }
